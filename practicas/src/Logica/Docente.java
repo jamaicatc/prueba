56dba5b55d7156e2f411cf1f4c1668c1ce6a88ee
Juan David <00jackhack11@gmail.com>
2025-03-22 01:20:50 -0500

    Main: creacion de metodos en clase docente

--- practicas/src/Logica/Docente.java
+++ practicas/src/Logica/Docente.java
@@ -1,5 +1,42 @@
 package Logica;
 
 public class Docente {
+    int id;
+    String nombre;
+    String apellido;
 
+    public Docente() {
+    }
+
+    public Docente(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    
 }
