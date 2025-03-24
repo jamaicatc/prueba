a22607492dc43db2d9b058a93d4397748479d28d
Santiago Trujillo <santiagotrujilloc7@gmail.com>
2025-03-24 00:04:44 -0500

    ultimos metodos de estudiante creados para completar el crud, creacion de objeto estudiante en el main para llamado de metodos

--- practicas/src/Logica/Estudiante.java
+++ practicas/src/Logica/Estudiante.java
@@ -1,6 +1,7 @@
 package Logica;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Estudiante {
@@ -74,6 +75,7 @@ public class Estudiante {
     }
 
     public void agregarEstudiante(){
+        scanner.useLocale(Locale.US); //explicacion del porque uso este metodo en la linea 96
         boolean condicion = true;
         while (condicion) {
             System.out.println("\npresiona 1 para agregar un estudiante, presiona 0 para salir: ");
@@ -91,7 +93,7 @@ public class Estudiante {
                     String apellido = scanner.nextLine();
         
                     System.out.println("\nIngrese la nota del estudiante: ");
-                    double nota = scanner.nextDouble();
+                    double nota = scanner.nextDouble(); //cuando se ingresa la nota del estudiante asi: 2.3 marca error, porque el scanner usa la configuracion regional de mi sistema, por eso en la linea 78 use el metodo .useLocale, porque si se pone la nota como: 2,3 se imprime despues como 2.3 y genera confusion
         
                     Estudiante nuevoEstudiante = new Estudiante(cedula, nombre, apellido, nota);
                     listaEstudiantes.add(nuevoEstudiante);
@@ -106,6 +108,7 @@ public class Estudiante {
         }
         
     }
+
     public void mostrarEstudiantes() {
         if (listaEstudiantes.isEmpty()) {
             System.out.println("\nNo hay estudiantes registrados.");
@@ -121,31 +124,56 @@ public class Estudiante {
         }
     }
 
-        public void actualizarNota(int cedula){
-            double nuevaNota;
-            if(listaEstudiantes.isEmpty()){
-                System.out.println("\nNo hay estudiantes registrados");
-            } else {
-                System.out.println("\ningrese la cedula del estudiante: ");
-                int cedIngresada = scanner.nextInt();
-                scanner.nextLine();
-                boolean encontrado = false;
-                for(Estudiante e: listaEstudiantes){
-                    if(e.getCedula() == cedIngresada){
-                        System.out.println("ingrese la nueva nota para el estudiante: ");
-                        nuevaNota = scanner.nextDouble();
-                        scanner.nextLine();
-                        e.setNota(nuevaNota);
-                        System.out.println("Nota actualizada correctamente");
-                        encontrado = true;
-                        break;
-                    }
-                }
-
-                if(!encontrado){
-                    System.out.println("error, estudiante no encontrado");   
-
+    public void actualizarNota(int cedula){
+        scanner.useLocale(Locale.US);
+        double nuevaNota;
+        if(listaEstudiantes.isEmpty()){
+            System.out.println("\nNo hay estudiantes registrados");
+        } else {
+            System.out.println("\ningrese la cedula del estudiante: ");
+            int cedIngresada = scanner.nextInt();
+            scanner.nextLine();
+            boolean encontrado = false;
+            for(Estudiante e: listaEstudiantes){
+                if(e.getCedula() == cedIngresada){
+                    System.out.println("\ningrese la nueva nota para el estudiante: ");
+                    nuevaNota = scanner.nextDouble();
+                    scanner.nextLine();
+                    e.setNota(nuevaNota);
+                    System.out.println("\nNota actualizada correctamente");
+                    encontrado = true;
+                    break;
                 }
             }
+            if(!encontrado){
+                System.out.println("error, estudiante no encontrado");   
+
+            }
+
         }
+    }
+
+    public void eliminarEstudiante(int cedula){
+        System.out.println("\ningrese la cedula del estudiante a eliminar: ");
+        int cedulaIngresada = scanner.nextInt();
+        boolean encontrado = false;
+        Estudiante estudianteAEliminar = null; //se almacena la cedula del estudiante en la variable, ya que no se puede eliminar o modificar elementos de una lista mientras el for each esta iterando sobre ella
+        if(listaEstudiantes.isEmpty()){
+            System.out.println("\nno hay estudiantes guardados");
+        } else{
+            for (Estudiante e : listaEstudiantes) {
+                if (cedulaIngresada == e.getCedula()) {
+                    estudianteAEliminar = e;
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (encontrado) {
+                listaEstudiantes.remove(estudianteAEliminar);
+                System.out.println("\nestudiante eliminado correctamente");
+            } else {
+                System.out.println("\nerror estudiante no encontrado");
+            }
+        }
+    }
 }
--- practicas/src/Logica/Main.java
+++ practicas/src/Logica/Main.java
@@ -2,27 +2,29 @@ package Logica;
 
 public class Main {
     public static void main(String[] args) {
-        //crear objetos de tipo estudiante (sirve tambien para crear objetos de otro tipo)
-        //tipo de dato(en este caso objeto estudiante) | nombre de la variable | = | new | nombre del objeto();
+        // crear objetos de tipo estudiante (sirve tambien para crear objetos de otro tipo)
+        // tipo de dato(en este caso objeto estudiante) | nombre de la variable | = | new | nombre del objeto();
 
-        Docente anaMaria = new Docente();
-        Docente juanDavid = new Docente(50, "Juan David", "Cardona");
+        // Docente anaMaria = new Docente();
+        // Docente juanDavid = new Docente(50, "Juan David", "Cardona");
 
-        System.out.println("El id del docente 1 es: " + juanDavid.getId());
-        System.out.println("El nombre del docente 1 es: " + juanDavid.getNombre() + "\nEl apellido del docente 1 es: " + juanDavid.getApellido());
+        // System.out.println("El id del docente 1 es: " + juanDavid.getId());
+        // System.out.println("El nombre del docente 1 es: " + juanDavid.getNombre() + "\nEl apellido del docente 1 es: " + juanDavid.getApellido());
 
-        anaMaria.setId(100);
-        anaMaria.setNombre("Ana Maria");
-        anaMaria.setApellido("Gomez");
+        // anaMaria.setId(100);
+        // anaMaria.setNombre("Ana Maria");
+        // anaMaria.setApellido("Gomez");
 
-        System.out.println("---------------------------------------");
-        System.out.println("El id del docente 1 es: " + anaMaria.getId());
-        System.out.println("El nombre del docente 1 es: " + anaMaria.getNombre() + "\nEl apellido del docente 1 es: " + anaMaria.getApellido());
+        // System.out.println("---------------------------------------");
+        // System.out.println("El id del docente 1 es: " + anaMaria.getId());
+        // System.out.println("El nombre del docente 1 es: " + anaMaria.getNombre() + "\nEl apellido del docente 1 es: " + anaMaria.getApellido());
 
         Estudiante alucno = new Estudiante();
         alucno.agregarEstudiante();
         alucno.mostrarEstudiantes();
         alucno.actualizarNota(0);
+        alucno.mostrarEstudiantes();
+        alucno.eliminarEstudiante(0);
         alucno.mostrarEstudiantes();
     }
 }
